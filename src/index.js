dcd1ce848cbea1a7a0e2a37b39b7db3ebbe023b3
Harsh <hy7486905@gmail.com>
2025-01-17 13:12:16 +0530

    Connect to Mongo atlas

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB() 
+
+/*
+import express from "express"
+const app = express()
+(async() =>{
+    try {
+       await mongoose.connect('${process.env.MONGODB_URL}/${DB_NAME}')
+
+    } catch (error) {
+        console.error("ERROR: " , error)
+        throw err
+    }
+})()
+*/
